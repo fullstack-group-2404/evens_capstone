ade4a139db20017787fb6a64641a27cb6f230c06
evensonm <evensonm1@gmail.com>
2024-10-29 14:13:46 -0400

    add

--- server/db/client.js
+++ server/db/client.js
@@ -1,6 +1,16 @@
 const pg = require("pg");
-const client = new pg.Client(
-  process.env.DATABASE_URL || "postgres://localhost/fsa_app_db"
-);
+
+// If it crashes locally it's because I only set up deploy configuration
+const client = new pg.Client({
+  // process.env.DATABASE_URL || "postgres://localhost/fsa_app_db"
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+  host: process.env.HOST,
+  ssl: true
+
+
+});
 
 module.exports = { client };
